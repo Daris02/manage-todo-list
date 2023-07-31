60f465994edb731df5a4164b1843099f758e2def
Daris02 <hei.raymond.2@gmail.com>
2023-07-31 18:36:01 +0300

    feat: add react hook form

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,26 +1,32 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function TaskForm({ tasks, setTasks }) {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  const { handleSubmit, register, formState: { errors } } = useForm();
+  function onSubmit(newTask) {
+    setTasks([...tasks, newTask]);
+  }
 
   return (
     <>
       <h2>Add new task : </h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" hidden={true} {...register("id")} value={Date.now()} />
         <input
           type="text"
           name="title"
           placeholder="Title"
           className="form-control m-2"
+          {...register("title")}
         />
         <input
           type="text"
           name="description"
           placeholder="Description"
           className="form-control m-2"
+          {...register("description")}
         />
+        <input type="text" hidden={true} {...register("isFinish")} value={false} />
         <button type="submit" className="btn btn-success w-100 m-2">
           Add
         </button>
