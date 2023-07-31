795688a2095429679d4d361507cc9dd17ccf50a2
Daris02 <hei.raymond.2@gmail.com>
2023-07-31 19:14:26 +0300

    fix: optimise object task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,17 +1,34 @@
 import React from "react";
+import EditForm from "./EditForm";
 
-export default function Task({ task, handleDelete }) {
+export default function Task({ task, handleDelete, handleEdit }) {
+  const Id = task.id;
+  const Title = task.title;
+  const Description = task.description;
 
   return (
     <>
       <div className="card m-2 p-2">
-        <h4>{task.title}</h4>
-        <p>{task.description}</p>
+        <h4>{Title}</h4>
+        <p>{Description}</p>
         <div className="card-footer d-flex justify-content-around">
-          <button className="btn btn-warning btn-sm">Edit</button>
-          <button className="btn btn-danger btn-sm" onClick={() => handleDelete(task.id)}>Delete</button>
+          <button
+            className="btn btn-warning btn-sm"
+            data-bs-toggle="modal"
+            data-bs-target="#editModal"
+          >
+            Edit
+          </button>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => handleDelete(Id)}
+          >
+            Delete
+          </button>
         </div>
       </div>
+
+      <EditForm task={task} handleEdit={handleEdit} />
     </>
   );
 }
