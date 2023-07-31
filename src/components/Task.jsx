7cb590a5dda78bc02b8b183fe20dd207fa91d6c7
Daris02 <hei.raymond.2@gmail.com>
2023-07-31 18:37:15 +0300

    feat: add button edit and delete

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-export default function Task({ task }) {
+export default function Task({ task, handleDelete }) {
+
   return (
     <>
-      <div className="container-fluid">
+      <div className="card m-2 p-2">
         <h4>{task.title}</h4>
         <p>{task.description}</p>
+        <div className="card-footer d-flex justify-content-around">
+          <button className="btn btn-warning btn-sm">Edit</button>
+          <button className="btn btn-danger btn-sm" onClick={() => handleDelete(task.id)}>Delete</button>
+        </div>
       </div>
     </>
   );
